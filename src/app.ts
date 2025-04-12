7b86b3472cdac9ed02b847dbefa2ad6f3629ca2e
Md Sajedul Islam <mdsajedulra@gmail.com>
2025-04-12 08:15:30 +0600

    cors policy update

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,20 @@ import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
 
 const app: Application = express();
 app.use(express.json());
-app.use(cors( {
-  origin: 'http://localhost:5173',  
-  credentials: true
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://yourfrontend.com'
+];
+
+app.use(cors( { origin: function (origin, callback) {
+  if (!origin || allowedOrigins.includes(origin)) {
+    callback(null, true);
+  } else {
+    callback(new Error('Not allowed by CORS'));
+  }
+},
+credentials: true
 }));
 
 app.use('/api', router);
