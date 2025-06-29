7d248e67db710679259e9b427af316c2d3a15cc7
Md Sajedul Islam <mdsajedulra@gmail.com>
2025-06-29 14:55:36 +0600

    first commit

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express';
 import userRoute from '../modules/user/user.route';
 import authrouter from '../modules/auth/auth.route';
-import productRoute from '../modules/product/product.route';
-import orderRoutes from '../modules/order/order.route';
+
 
 const router = Router();
 
@@ -15,14 +14,7 @@ const moduleRoutes = [
     path: '/auth',
     route: authrouter,
   },
-  {
-    path: '/product',
-    route: productRoute,
-  },
-  {
-    path: '/order',
-    route: orderRoutes,
-  },
+  
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
